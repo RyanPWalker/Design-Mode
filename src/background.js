1eaf92b6530dd86e32c6b2e70d2e64a7fecf2a40
Ryan Walker <rywalker@overstock.com>
2019-10-13 15:49:33 -0600

    Added functionality for disabling buttons and anchor tags on extension activation

--- src/background.js
+++ src/background.js
@@ -2,4 +2,5 @@
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript(null, {file: "src/toggleDesignMode.js"});
+  chrome.tabs.executeScript(null, {file: "src/disableButtons.js"});
 });
