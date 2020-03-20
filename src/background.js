61645ac26b8b94bfc313b0177450c3dec07572e0
Ryan Walker <rywalker@overstock.com>
2020-03-19 22:36:35 -0600

    1.2.0 Added new greyscale icons for when design mode is disabled and logic for toggling the icon displayed.

--- src/background.js
+++ src/background.js
@@ -1,5 +1,19 @@
 'use strict';
 
+chrome.extension.onMessage.addListener(function (request) {
+  var currentDesignMode = request.currentDesignMode;
+  var iconPath = currentDesignMode === 'on' ? 'cursor' : 'greyscaleCursor';
+
+  chrome.browserAction.setIcon({
+      path: {
+          "16": "images/" + iconPath + "16.png",
+          "32": "images/" + iconPath + "32.png",
+          "64": "images/" + iconPath + "64.png",
+          "128": "images/" + iconPath + "128.png"
+      }
+  });
+});
+
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript(null, {file: "src/utils/getChromeVersion.js"}); // functions called by other files need to be loaded first
   chrome.tabs.executeScript(null, {file: "src/toggleDesignMode.js"});
--- src/toggleDesignMode.js
+++ src/toggleDesignMode.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var currentDesignMode = '';
 // Before Chrome 64, designMode was accessed through the HTMLDocument alias.
 // designMode doesn't exist before 45
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/designMode#Browser_compatibility
 if (getChromeVersion() < 64) {
   HTMLDocument.designMode = HTMLDocument.designMode === 'on' ? 'off' : 'on';
+  currentDesignMode = HTMLDocument.designMode;
 } else {
   document.designMode = document.designMode === 'on' ? 'off' : 'on';
+  currentDesignMode = document.designMode;
 }
+
+chrome.extension.sendMessage({
+  "currentDesignMode": currentDesignMode
+});
